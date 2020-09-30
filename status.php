2c8655c8ed17a9dfff3e58b459d53f2da9ee3b3c
CalistoX <68951062+CalistoX@users.noreply.github.com>
2020-09-30 10:45:28 +0200

    Icons updated

--- status.php
+++ status.php
@@ -7,8 +7,8 @@ if (!isset($_SESSION['loggedin'])) {
 }
 
 include_once('config_status.php');
-$result = $con->prepare('SELECT id, temperatur, luftfeuchtigkeit, timestamp FROM `dht11`');
-$result->bind_result($id, $temperatur, $luftfeuchtigkeit, $timestamp);
+$result = $con->prepare('SELECT id, temperatur, luftfeuchtigkeit, zeitpunkt FROM `dht11`');
+$result->bind_result($id, $temperatur, $luftfeuchtigkeit, $zeitpunkt);
 $result->fetch();
 $result->close();
 ?>
@@ -25,21 +25,21 @@ $result->close();
 			<nav class="navtop">
 			<div>
                 <h1>Yokai Serverüberwachung</h1>
-				<a href="cctv.html"><i class="fa fa-video-camera" aria-hidden="true"></i>CCTV</a>
-                <a href="status.php"><i class="fa fa-server" aria-hidden="true"></i>Status</a>
-				<a href="profile.php"><i class="fas fa-user-circle" aria-hidden="true"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt" aria-hidden="true"></i>Logout</a>
+				<a href="cctv.html"><i class="fa fa-video-camera fa-fw" aria-hidden="true"></i>CCTV</a>
+                <a href="status.php"><i class="fa fa-server fa-fw" aria-hidden="true"></i>Status</a>
+				<a href="profile.php"><i class="fas fa-user-circle fa-fw" aria-hidden="true"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt fa-fw" aria-hidden="true"></i>Logout</a>
 			</div>
 		</nav>
         <div class="content">
             <h2>Server Status <i class="fa fa-server" aria-hidden="true"></i></h2>
 			<div class="tempStatus">
 				<h2>Temperatur</h2>
-				<p>> <?= $temperatur?>C &deg; <inline class="trennStrich">|</inline> <?= $timestamp?></p>
+				<p>> <?= $temperatur?>C &deg; <inline class="trennStrich">|</inline> <?= $zeitpunkt?></p>
 			</div>
 			<div class="humidityStatus">
 				<h2>Luftfeuchtigkeit</h2>
-				<p>> <?= $luftfeuchtigkeit?>% <inline class="trennStrich">|</inline> <?= $timestamp?></p>
+				<p>> <?= $luftfeuchtigkeit?>% <inline class="trennStrich">|</inline> <?= $zeitpunkt?></p>
 			</div>
 		</div>
         </body>
